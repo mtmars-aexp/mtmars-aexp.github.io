6aa3f8d3eb42083a4f98d2acc4667162c88a8f18
Morgan.Mars@aexp.com <morgan.t.mars@aexp.com>
2020-03-04 14:59:56 +0000

    revamped the entire dealio

--- code.js
+++
@@ -1,5 +0,0 @@
-function secretButton(){
-
-    alert("oh shit son you found the SECRET NUMBER BUTTON");
-
-}
---
+++ js/code.js
@@ -0,0 +1,9 @@
+function secretButton(){
+
+    alert("oh shit son you found the SECRET NUMBER BUTTON");
+
+}
+
+function loadComponent(componentName){
+    $("#replace-"+componentName).load("components/"+componentName+".html")
+}
